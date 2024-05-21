d45a5a55c0e80ad3ad69a92a95135574648842ad
Clement Couturier <ccouturier@b2pweb.com>
2024-05-21 11:01:26 +0200

    [+] Ajout des paramètres du calendrier

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,5 +1,4 @@
 import { Calendar } from 'fullcalendar';
-import dayGridPlugin from '@fullcalendar/daygrid';
 import iCalendarPlugin from '@fullcalendar/icalendar';
 import ICAL from 'ical.js';
 // console.log(window.calendarUrls);
@@ -7,8 +6,33 @@ import ICAL from 'ical.js';
 document.addEventListener("livewire:initialized", function () {
     let calendarEl = document.querySelector("#calendar");
     var calendar = new Calendar(calendarEl, {
-        plugins: [dayGridPlugin, iCalendarPlugin],
-        eventSources: window.calendarUrls.map(url => ({ url: url, format: 'ics' }))
+        plugins: [iCalendarPlugin],
+        editable: true,
+        selectable: true,
+        droppable: true,
+        selectMirror: true,
+        locale: "fr",
+        weekNumberCalculation: "ISO",
+        initialView: "timeGridWeek",
+        headerToolbar: {
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
+        },
+        views: {
+            dayGridMonth: {
+                fixedWeekCount: false,
+            },
+        },
+        eventSources: window.calendarUrls.map((url) => ({
+            url: url,
+            format: "ics",
+        })),
+
+        select: function (info) {
+            console.log(info);
+            openModal();
+        },
     });
 
     calendar.render();
