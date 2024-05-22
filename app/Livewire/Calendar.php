6945e7711f0571f1232fbe6185ee5a7b5340ade4
Clement Couturier <ccouturier@b2pweb.com>
2024-05-22 10:20:06 +0200

    [~] Autoformat

--- app/Livewire/Calendar.php
+++ app/Livewire/Calendar.php
@@ -12,7 +12,7 @@ use ICal\ICal;
 class Calendar extends Component
 {
     public $events = [];
-    public $allUrlIcsEvents= [];
+    public $allUrlIcsEvents = [];
     public $calendarUrl;
 
     /**
@@ -22,42 +22,28 @@ class Calendar extends Component
      */
     public function render()
     {
-        
-        
-        $laravelSabreRoot= config('app.laravelSabreRoot');
-        $appRoot= config('app.appRoot');
-        $user= auth()->user();
-        $userName= $user->username;
-        $hashUserName= $user->hashUserName();
-        $calendar= DB::table('calendarinstances')->where('principaluri', 'LIKE', '%/' . $hashUserName)->first();
+        $laravelSabreRoot = config('app.laravelSabreRoot');
+        $appRoot = config('app.appRoot');
+        $user = auth()->user();
+        $userName = $user->username;
+        $hashUserName = $user->hashUserName();
+        $calendar = DB::table('calendarinstances')->where('principaluri', 'LIKE', '%/' . $hashUserName)->first();
+
         if (!$calendar) {
-            
+
             $this->events = Events::all();
             return view('livewire.calendar');
         }
+
         $calendarId = $calendar->calendarid;
-        $this->calendarUrl= $appRoot . '/' . $laravelSabreRoot . '/calendars' . '/' . $hashUserName . '/' . $calendar->uri;
-        // dd($this->calendarUrl);
+        $this->calendarUrl = $appRoot . '/' . $laravelSabreRoot . '/calendars' . '/' . $hashUserName . '/' . $calendar->uri;
         $calendarobjectsUser = Calendarobject::where('calendarid', $calendarId)->get();
-        foreach($calendarobjectsUser as $calendarobject){
-            $ics= $calendarobject->uri; //cest les données brut d'un fichier ics la 
-            $this->allUrlIcsEvents[]=$this->calendarUrl . '/' . $ics;
+        
+        foreach ($calendarobjectsUser as $calendarobject) {
+            $ics = $calendarobject->uri; //cest les données brut d'un fichier ics la 
+            $this->allUrlIcsEvents[] = $this->calendarUrl . '/' . $ics;
         }
-      
-        
 
-
-        // dd($ics);
-
-
-
-        
-
-
-
-
-
-        
         return view('livewire.calendar');
     }
 }
