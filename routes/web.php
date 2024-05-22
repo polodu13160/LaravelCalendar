2ce2b39de39c635e78e3996f2c1bea27c7e5bc53
Clement Couturier <ccouturier@b2pweb.com>
2024-05-22 10:20:40 +0200

    [+] Ajout d'une route de test iCal

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Livewire\EventComponent;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DAVController;
 use App\Http\Middleware\AccesSabreJustAdmin;
+use Spatie\IcalendarGenerator\Components\Calendar as ComponentsCalendar;
+use Spatie\IcalendarGenerator\Components\Event;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,6 +16,22 @@ Route::get('/', function () {
 Route::get('/dd', function () {
     $fetch = new EventComponent();
     dd($fetch->refetchEvents());
+});
+
+Route::get('/ddical', function () {
+    $calendar = ComponentsCalendar::create('Laracon Online');
+    $event = Event::create()
+        ->name('Laracon Event')
+        ->startsAt(new DateTime('26 may 2024 15:00'))
+        ->endsAt(new DateTime('26 may 2024 16:00'));
+    $event2 = Event::create()
+        ->name('Laracon Event 2')
+        ->startsAt(new DateTime('27 may 2024 15:00'))
+        ->endsAt(new DateTime('27 may 2024 16:00'));
+    $calendar->event([$event, $event2]);
+
+    return response($calendar->get())
+        ->header('Content-Type', 'text/calendar; charset=utf-8');
 });
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
