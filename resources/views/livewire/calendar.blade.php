98e2a2de30228e4a7a218f7c145b9f16e72468a1
Clement Couturier <ccouturier@b2pweb.com>
2024-05-22 10:20:15 +0200

    [~] Autoformat

--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -1,13 +1,12 @@
 <div>
     <div>{{ $this->calendarUrl }}</div>
     <div>
-       <script>
-        window.calendarUrls = @json($this->allUrlIcsEvents);
+        <script>
+            window.calendarUrls = @json($this->allUrlIcsEvents);
         </script>
         <div id="calendar-container" wire:ignore>
             <div id="calendar"></div>
         </div>
         @vite(['resources/js/calendar.js'])
     </div>
-    
 </div>
