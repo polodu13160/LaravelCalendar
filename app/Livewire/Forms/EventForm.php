156819530ab6724af549be48de7e9658144014cb
Clement Couturier <ccouturier@b2pweb.com>
2024-05-23 10:20:56 +0200

    [+] Les events sont bien "public" ou "private"

--- app/Livewire/Forms/EventForm.php
+++ app/Livewire/Forms/EventForm.php
@@ -49,12 +49,14 @@ class EventForm extends Form
 
     public function store(): void
     {
+        $this->visibility = $this->visibility === '1' ? 'private' : 'public';
         // $this->validate();
         Events::create($this->only(['title', 'description', 'event_id', 'user_id', 'start', 'end', 'status', 'is_all_day', 'visibility']));
     }
 
     public function update()
     {
+        $this->visibility = $this->visibility === '1' ? 'private' : 'public';
         $this->events->
         update($this->only(['title', 'description', 'event_id', 'user_id', 'start', 'end', 'status', 'is_all_day', 'visibility']));
     }
