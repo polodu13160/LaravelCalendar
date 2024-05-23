b062eef939624f693c1be6d5ac72d1fcff298f3c
Clement Couturier <ccouturier@b2pweb.com>
2024-05-23 17:32:54 +0200

    [+] Ajout d'une fonction pour créer des events iCal

--- app/Livewire/Forms/EventForm.php
+++ app/Livewire/Forms/EventForm.php
@@ -3,8 +3,13 @@
 namespace App\Livewire\Forms;
 
 use App\Models\Events;
+use DateTime;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\DB;
 use Livewire\Form;
+use Spatie\IcalendarGenerator\Components\Calendar;
+use Spatie\IcalendarGenerator\Components\Event;
+use Spatie\IcalendarGenerator\Enums\Classification;
 
 class EventForm extends Form
 {
@@ -24,7 +29,7 @@ class EventForm extends Form
 
     // #[Validate('required')]
     public string $end = '';
-    
+
     public string $description = '';
     public int $status = 0;
     public int $is_all_day = 0;
@@ -50,27 +55,65 @@ class EventForm extends Form
 
     public function store(): void
     {
-        $this->storeiCalEvent();
         $this->visibility = $this->visibility === '1' ? 'private' : 'public';
-        // $this->validate();
+
         Events::create($this->only(['title', 'description', 'event_id', 'user_id', 'start', 'end', 'status', 'is_all_day', 'visibility']));
+        $this->storeiCalEvent();
     }
 
-    public function storeiCalEvent() {
-
+    public function storeiCalEvent()
+    {
         // Créer un nouveau client Guzzle
         $client = new Client();
 
-        $url = $this->calendarUrl . $this->title . ".ics";
-        dd($this->calendarUrl);
-        // Définir l'URL de l'événement sur le serveur Sabre/dav
-        // $url = 'http://localhost/dav/calendars/6fa5bf2dd665bfd42687/lecalendrierdeAdmin/LaraconOnline.ics';
+        $user = auth()->user();
+        $hashUserName = $user->hashUserName();
+        
+        $hashTitle = md5($this->title);
+
+        $laravelSabreRoot = config('app.laravelSabreRoot');
+        $appRoot = config('app.appRoot');
+        $calendar = DB::table('calendarinstances')->where('principaluri', 'LIKE', '%/' . $hashUserName)->first();
+        
+        $url = $appRoot . '/' . $laravelSabreRoot . '/calendars' . '/' . $hashUserName . '/' . $calendar->uri . '/' . $hashTitle . ".ics";
+
+        $classification = $this->classification($this->visibility);
+
+        $test = Event::create()
+        ->name($this->title)
+        ->description($this->description)
+        ->uniqueIdentifier($this->event_id)
+        ->classification($classification)
+        ->createdAt(new DateTime('now'))
+            ->startsAt(new DateTime($this->start))
+            ->endsAt(new DateTime($this->end));
+
+        $cal = Calendar::create()->event($test)->get();
+        
+        $response = $client->request('PUT', $url, [
+            'body' => $cal,
+            'headers' => [
+                'Content-Type' => 'text/calendar; charset=UTF-8',
+                'If-None-Match' => '*',
+            ],
+        ]);
     }
+
+    public function classification($value) {
+
+        if ($value === 'public') {
+            return Classification::public();
+        } else if ($value === 'private') {
+            return Classification::private();
+        } else {
+            return Classification::confidential();
+        }
+    }
+
 
     public function update()
     {
         $this->visibility = $this->visibility === '1' ? 'private' : 'public';
-        $this->events->
-        update($this->only(['title', 'description', 'event_id', 'user_id', 'start', 'end', 'status', 'is_all_day', 'visibility']));
+        $this->events->update($this->only(['title', 'description', 'event_id', 'user_id', 'start', 'end', 'status', 'is_all_day', 'visibility']));
     }
 }
