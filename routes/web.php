f127787c24d2f0ca747dd6c60436a7010f55364e
Clement Couturier <ccouturier@b2pweb.com>
2024-05-23 12:38:40 +0200

    [+] WIP ics

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Livewire\EventComponent;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DAVController;
 use App\Http\Middleware\AccesSabreJustAdmin;
+use GuzzleHttp\Client;
+use Sabre\VObject\Component\VCalendar;
 use Spatie\IcalendarGenerator\Components\Calendar as ComponentsCalendar;
 use Spatie\IcalendarGenerator\Components\Event;
 
@@ -16,6 +18,45 @@ Route::get('/', function () {
 Route::get('/dd', function () {
     $fetch = new EventComponent();
     dd($fetch->refetchEvents());
+});
+
+Route::get('/dd2', function () {
+
+    // Créer un nouveau client Guzzle
+    $client = new Client();
+
+    // Définir l'URL de l'événement sur le serveur Sabre/dav
+    $url = 'http://localhost/dav/calendars/6fa5bf2dd665bfd42687/lecalendrierdeAdmin/LaraconOnline.ics';
+
+    $test = Event::create()
+        ->name('Laracon Online')
+        ->description('Experience Laracon all around the world')
+        ->uniqueIdentifier('A unique identifier can be set here')
+        ->createdAt(new DateTime('6 march 2024'))
+        ->startsAt(new DateTime('6 march 2024 15:00'))
+        ->endsAt(new DateTime('6 march 2024 16:00'));
+
+        $cal = ComponentsCalendar::create()->event($test)->get();
+
+    // Définir le contenu de l'événement au format iCalendar
+    // $icsContent = 'BEGIN:VCALENDAR
+    //                 VERSION:2.0
+    //                 BEGIN:VEVENT
+    //                 UID:123456
+    //                 DTSTART:20220101T100000Z
+    //                 DTEND:20220101T120000Z
+    //                 SUMMARY:Mon événement
+    //                 END:VEVENT
+    //                 END:VCALENDAR';
+
+    // Envoyer la requête PUT
+    $response = $client->request('PUT', $url, [
+        'body' => $cal,
+        'headers' => [
+            'Content-Type' => 'text/calendar; charset=UTF-8',
+            'If-None-Match' => '*',
+        ],
+    ]);
 });
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
