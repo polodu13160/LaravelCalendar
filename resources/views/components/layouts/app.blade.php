d61c6c41bd0cd33476f0e36d26d943e45cd2b4a0
Clement Couturier <ccouturier@b2pweb.com>
2024-05-24 14:55:32 +0200

    [~] Changement du nom de l'app

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <title>{{ $title ?? "Page Title" }}</title>
+        <title>{{ $title ?? "B2P Calendar" }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net" />
