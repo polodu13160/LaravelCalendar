5415eb7da52d1e078bef6765afd5d1f1853b1d53
Clement Couturier <ccouturier@b2pweb.com>
2024-05-24 14:56:04 +0200

    [~] WIP Changement du logo Jetstream

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -8,7 +8,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-mark class="block h-9 w-auto" />
+                        <x-b2p-logo class="block h-9 w-auto" />
                     </a>
                 </div>
 
