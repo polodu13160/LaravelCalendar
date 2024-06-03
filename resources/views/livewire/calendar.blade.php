a49aab25740e924c5c15f80739567cff4bf79535
Clement Couturier <ccouturier@b2pweb.com>
2024-06-03 12:04:09 +0200

    [+] Ajout d'un header

--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -1,3 +1,8 @@
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Calendrier') }}
+        </h2>
+    </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 border-box">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg max-h-90% border-box">
