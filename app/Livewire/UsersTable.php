346d65f3712e5a9610bb230bb5538d0fc8d0f0c1
Clement Couturier <ccouturier@b2pweb.com>
2024-06-05 16:29:30 +0200

    [~] Autoformat

--- app/Livewire/UsersTable.php
+++ app/Livewire/UsersTable.php
@@ -13,17 +13,13 @@ class UsersTable extends TeamMemberManager
 {
     public function updateSearch()
     {
-
         $this->render();
-
     }
 
     public function setFormValues($role, $email)
     {
         $this->addTeamMemberForm['role'] = $role;
         $this->addTeamMemberForm['email'] = $email;
-
-        // dd($this->addTeamMemberForm['role'] ,$this->addTeamMemberForm['email']);
     }
 
     /**
@@ -70,18 +66,11 @@ class UsersTable extends TeamMemberManager
                 $newTeamInvitation->user_id_createInvite = auth()->user()->id;
                 $newTeamInvitation->save();
             } catch (\Throwable $th) {
-                //    dd($th);
+
                 if ($th->getCode() == 23000) {
                     $this->addError('addTeamMemberForm', 'Cet utilisateur est déja dans l\'équipe');
                 }
             }
-
-            // app(InvitesTeamMembers::class)->invite(
-            //     $this->user,
-            //     $this->team,
-            //     $this->addTeamMemberForm['email'],
-            //     $this->addTeamMemberForm['role']
-            // );
         }
 
         $this->addTeamMemberForm = [
@@ -96,13 +85,9 @@ class UsersTable extends TeamMemberManager
 
     public function setRole($role, $user, $teamId)
     {
-        // dd($role);
         $teamUser = User::find($user)->teams->find($teamId)->membership;
-        // $teamUser=User::find($user)->teams->find($teamId)->membership->first();
-        // dd($success);
         $teamUser->role = $role;
         $teamUser->save();
-        // dd('ok');
         $this->dispatch('good');
     }
 
@@ -114,7 +99,6 @@ class UsersTable extends TeamMemberManager
             $userTeamNewLeader = User::find($user_id)->teams->find($this->team->id)->membership;
             $userTeamNewLeader->role = 2;
             $userTeamNewLeader->save();
-            // dd($userTeamNewLeader);
 
             $this->team->user_id = $user_id;
             $this->team->save();
@@ -125,7 +109,6 @@ class UsersTable extends TeamMemberManager
 
             return;
         }
-
     }
 
     /**
@@ -135,10 +118,12 @@ class UsersTable extends TeamMemberManager
      */
     public function render()
     {
-
         return view('teams.team-member-manager', [
             'users' => User::where('email', 'LIKE', "%{$this->addTeamMemberForm['email']}%")->where('id', '!=', auth()->user()->id)->where(
-                'name', '!=', 'Admin')->get(),
+                'name',
+                '!=',
+                'Admin'
+            )->get(),
             'roleTest' => Role::where('name', '!=', 'Admin')->get(),
         ]);
     }
