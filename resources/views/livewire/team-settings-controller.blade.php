ff7914be61108483e2a49d6a709e31a6370ded81
Clement Couturier <ccouturier@b2pweb.com>
2024-06-05 16:31:56 +0200

    [~] Autoformat

--- resources/views/livewire/team-settings-controller.blade.php
+++ resources/views/livewire/team-settings-controller.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            <livewire:teams.update-team-name-form :$team :$isModerator/>
+            <livewire:teams.update-team-name-form :$team/>
 
             @livewire('users-table', ['team' => $team])
 
