9460a9517354f378bf104531f2ce9a9d08421269
Clement Couturier <ccouturier@b2pweb.com>
2024-06-06 16:06:43 +0200

    [~] Correction de l'affichage des actions

--- resources/views/components/action-section.blade.php
+++ resources/views/components/action-section.blade.php
@@ -10,7 +10,7 @@
         </div>
         @if (isset($actions))
         <div
-            class="flex items-center justify-end px-4 py-3 bg-gray-50 text-end sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+        class="flex items-center justify-end px-4 py-3 bg-none text-end sm:px-6 sm:rounded-bl-md sm:rounded-br-md">
             {{ $actions }}
         </div>
         @endif
