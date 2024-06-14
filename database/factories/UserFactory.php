16ce477d264f6b5e7d14d6af5f2f2856cc6d2fc8
Clement Couturier <ccouturier@b2pweb.com>
2024-06-14 10:00:06 +0200

    [+] Ajout de la colonne color sur la table users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -34,6 +34,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => Hash::make($password),
+            'color' => fake()->hexColor,
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'remember_token' => Str::random(10),
