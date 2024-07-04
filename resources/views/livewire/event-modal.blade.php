fdba4e0928e12122d4f3184641b510fc92aeb69b
Clement Couturier <ccouturier@b2pweb.com>
2024-07-04 15:57:24 +0200

    [+] Rajout de category et d'un if sur le submit du form (update ou save)

--- resources/views/livewire/event-modal.blade.php
+++ resources/views/livewire/event-modal.blade.php
@@ -1,25 +1,21 @@
 <div class="p-6">
-    <form wire:submit="update" >
+    @if ($this->events)
+    <form wire:submit="update">
+    @else
+    <form wire:submit="save">
+    @endif
         <!-- title input -->
         <div>
             <x-label for="title" :value="__('Titre')" />
-            <x-input
-                wire:model="form.title"
-                id="title"
-                class="mt-1 block w-full"
-                type="text"
-            />
+            <x-input wire:model="form.title" id="title" class="mt-1 block w-full" type="text" />
             <x-input-error for="form.title" class="mt-2" />
         </div>
         <!-- Description input -->
         <div class="mt-4">
             <x-label for="description" :value="__('Description')" />
-            <textarea
-                wire:model="form.description"
-                id="description"
-                class="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-            ></textarea>
-           <x-input-error for="form.description" class="mt-2" />
+            <textarea wire:model="form.description" id="description"
+                class="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"></textarea>
+            <x-input-error for="form.description" class="mt-2" />
         </div>
         <!-- is_all_day input -->
         <div>
@@ -29,33 +25,35 @@
         <!-- visibility checkbox -->
         <div>
             <x-label for="visibility" :value="__('Visibilité')" />
-            <select wire:model="form.visibility" id="visibility" name="visibility" class="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
+            <select wire:model="form.visibility" id="visibility" name="visibility"
+                class="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
                 <option value="public">Public</option>
                 <option value="private">Privé</option>
                 <option value="confidential">Confidentiel</option>
             </select>
             <x-input-error for="form.visibility" class="mt-2" />
         </div>
+        <!-- category checkbox -->
+        <div>
+            <x-label for="category" :value="__('Catégorie')" />
+            <select wire:model="form.category" id="category" name="category"
+                class="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
+                <option value="RDV">RDV</option>
+                <option value="Appels">Appels</option>
+                <option value="test">Test (doit renvoyer une erreur)</option>
+            </select>
+            <x-input-error for="form.category" class="mt-2" />
+        </div>
         <!-- start input -->
         <div>
             <x-label for="start" :value="__('Début')" />
-            <x-input
-                wire:model="form.start"
-                id="start"
-                class="mt-1 block w-full"
-                type="datetime-local"
-            />
-           <x-input-error for="form.start" class="mt-2" />
+            <x-input wire:model="form.start" id="start" class="mt-1 block w-full" type="datetime-local" />
+            <x-input-error for="form.start" class="mt-2" />
         </div>
         <!-- end input -->
         <div>
             <x-label for="end" :value="__('Fin')" />
-            <x-input
-                wire:model="form.end"
-                id="end"
-                class="mt-1 block w-full"
-                type="datetime-local"
-            />
+            <x-input wire:model="form.end" id="end" class="mt-1 block w-full" type="datetime-local" />
             <x-input-error for="form.end" class="mt-2" />
         </div>
         <!-- Save button -->
@@ -64,3 +62,4 @@
         </div>
     </form>
 </div>
+
