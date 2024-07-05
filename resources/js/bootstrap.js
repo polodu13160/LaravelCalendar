3ed202ee205b722ea726264d11ed419acb933631
Clement Couturier <ccouturier@b2pweb.com>
2024-07-05 11:30:02 +0200

    [+] Import de Tippy

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,9 @@
 import axios from 'axios';
 window.axios = axios;
 
+import tippy from 'tippy.js';
+window.tippy = tippy;
+import 'tippy.js/dist/tippy.css'; // optional for styling
+import "tippy.js/themes/material.css";
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
