df0fb647269ae4b0a57ccdda7b30e69944b39173
Clement Couturier <ccouturier@b2pweb.com>
2024-07-05 11:32:15 +0200

    [~] - Suppression des functions liées aux ICS ~ Remplacement du tooltip par Tippy ~ WIP Pour le moment, FullCalendar est invoqué avec CDN

--- resources/views/livewire/calendar-component.blade.php
+++ resources/views/livewire/calendar-component.blade.php
@@ -27,96 +27,83 @@
                         </div>
                     </div>
 
-                    @vite(['resources/js/calendar.js'])
-                    @script
-                        <script>
-                            let calendar;
-                            let users;
-                            let team;
-                            let all;
+                    <script src="https://cdn.jsdelivr.net/npm/fullcalendar/index.global.min.js"></script>
+                    <script>
+                        let calendar;
 
-                            document.addEventListener("livewire:initialized", initializeCalendar);
-
-                            function initializeCalendar() {
-                                let tooltip = document.createElement('div');
-                                tooltip.style.position = 'absolute';
-                                tooltip.style.backgroundColor = 'white';
-                                tooltip.style.border = '1px solid black';
-                                tooltip.style.padding = '10px';
-                                tooltip.style.width = '400px';
-                                tooltip.style.zIndex = '10000';
-                                tooltip.style.display = 'none';
-                                document.body.appendChild(tooltip);
-                                let currentEvent = null;
-
-                                let calendarEl = document.querySelector("#calendar");
-
-                                calendar = new window.Calendar(calendarEl, {
-                                    plugins: [window.iCalendarPlugin],
-                                    editable: true,
-                                    selectable: true,
-                                    droppable: true,
-                                    selectMirror: true,
-                                    locale: "fr",
-                                    weekNumberCalculation: "ISO",
-                                    initialView: "timeGridWeek",
-                                    headerToolbar: {
-                                        left: "prev,next today",
-                                        center: "title",
-                                        right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+                        document.addEventListener("livewire:initialized", function() {
+                            let calendarEl = document.querySelector("#calendar");
+                            calendar = new FullCalendar.Calendar(calendarEl, {
+                                editable: true,
+                                selectable: true,
+                                droppable: true,
+                                selectMirror: true,
+                                aspectRatio: 1.5,
+                                locale: "fr",
+                                weekNumberCalculation: "ISO",
+                                initialView: "timeGridWeek",
+                                headerToolbar: {
+                                    left: "prev,next today",
+                                    center: "title",
+                                    right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+                                },
+                                views: {
+                                    dayGridMonth: {
+                                        fixedWeekCount: false,
+                                        showNonCurrentDates: true,
                                     },
-                                    views: {
-                                        dayGridMonth: {
-                                            fixedWeekCount: false,
-                                        },
-                                    },
-                                    eventMouseEnter: function(info) {
-                                        currentEvent = info.event;
-                                        fetchEventData(info.event.source.url).then(iCalText => {
-                                            let iCalContent = parseICalContent(iCalText);
-                                            tooltip.innerHTML = `<strong>${info.event.title}</strong><br>
-                                                <strong>Date debut </strong> : ${info.event.start}<br>
-                                                <strong>Date fin </strong> : ${info.event.end}<br>
-                                                <strong>Description </strong> : ${info.event.extendedProps.description}<br>
-                                                <strong>Catégories </strong> : ${iCalContent.CATEGORIES}<br>`;
+                                },
 
-                                            let rect = info.el.getBoundingClientRect();
-                                            tooltip.style.left = (window.scrollX + rect.right) + 'px';
-                                            tooltip.style.top = (window.scrollY + rect.top) + 'px';
-                                            tooltip.style.display = 'block';
-                                            info.el.addEventListener('mouseleave', function() {
-                                                tooltip.style.display = 'none';
-                                                currentEvent = null;
-                                            });
+                                eventMouseEnter: function(info) {
+                                    let event = info.event;
+                                    console.log(event);
+                                    var tooltip = window.tippy(info.el, {
+                                        interactive: true,
+                                        appendTo: () => document.body,
+                                        placement: 'auto',
+                                        delay: 300,
+                                        theme: 'material',
+                                        allowHTML: true,
+                                        content: event.title +
+                                        '<br>' +
+                                        event.extendedProps.description +
+                                        '<br>' +
+                                        event.extendedProps.category +
+                                        '<br>' +
+                                        event.start.toLocaleString() +
+                                        '<br>' +
+                                        event.end.toLocaleString() +
+                                        '<br>'
+                                        ,
+                                    });
+                                },
 
-                                        });
-                                    },
-                                    eventResize: function(info) {
-                                        @this.updateEvent(info.event.id, info.event.startStr, info.event.endStr);
-                                    },
+                                eventResize: function(info) {
+                                    @this.updateEvent(info.event.id, info.event.startStr, info.event.endStr);
+                                },
 
-                                    eventDrop: function(info) {
-                                        @this.updateEvent(info.event.id, info.event.startStr, info.event.endStr, info.event.allDay);
-                                    },
+                                eventDrop: function(info) {
+                                    @this.updateEvent(info.event.id, info.event.startStr, info.event.endStr, info.event
+                                        .allDay);
+                                },
 
-                                    select: function(info) {
-                                        console.log(info);
-                                        openNewModal();
-                                    },
-                                    eventClick: function(info) {
-                                        openModal(info);
-                                    },
-                                });
+                                select: function(info) {
+                                    console.log(info);
+                                    openNewModal();
+                                },
+                                eventClick: function(info) {
+                                    openModal(info);
+                                },
+                            });
 
-                                calendar.render();
-                            }
+                            calendar.render();
 
                             Livewire.on("eventsHaveBeenFetched", () => {
 
                                 calendar.removeAllEventSources();
 
                                 calendar.addEventSource(fetchJSONEvents());
-                                
+
                                 console.log("Events have been fetched");
                             });
 
@@ -143,26 +130,8 @@
                             function fetchJSONEvents() {
                                 return JSON.parse(@this.events);
                             }
-
-                            function parseICalContent(iCalContent) {
-
-                                let lines = iCalContent.split('\n');
-
-                                let iCalObject = lines.reduce((acc, line) => {
-                                    let [key, ...value] = line.split(':');
-                                    acc[key] = value.join(':').trim();
-                                    return acc;
-                                }, {});
-                                return iCalObject;
-                            }
-
-                            async function fetchEventData(url) {
-                                const response = await fetch(url);
-                                const data = await response.text();
-                                return data;
-                            }
-                        </script>
-                    @endscript
+                        });
+                    </script>
                 </div>
             </div>
         </div>
