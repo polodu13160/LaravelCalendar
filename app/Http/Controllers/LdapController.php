cd63f95dcc1272e94cb9e42620d001eae4402117
polodu13160 <paul.depetrini@gmail.com>
2024-08-09 17:01:33 +0200

    creation d'un ldap controller
    pour faire un test de connexion
    cela marche mais qu'avec un CN non un uid
    
    a faire sur le provider

--- app/Http/Controllers/LdapController.php
+++ app/Http/Controllers/LdapController.php
@@ -2,18 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use LdapRecord\Container;
+use LdapRecord\Connection;
 use Illuminate\Http\Request;
-use LdapRecord\Models\ActiveDirectory\User;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Config;
+use LdapRecord\Models\ActiveDirectory\User;
 
 class LdapController extends Controller
 {
     public function search($uid)
     {
+        $password = 'LdapDeV123456++';
+        $baseDn = Config::get('ldap.connections.default.base_dn');
+        $username = "uid=$uid,$baseDn";
+
+        // $username = "CN=ldapdev,OU=DEV,OU=Technique,OU=SAMDOM_Utilisateurs,DC=samdom,DC=b2pweb,DC=com" ;
+        $config = Config::get('ldap.connections.default');
+
+        // Modifier la configuration avec les identifiants dynamiques
+        $config['username'] = $username;
+        $config['password'] = $password;
+
+
+
+        // Créer une nouvelle connexion LDAP avec la configuration mise à jour
+        $connection = new Connection($config);
+
+        // Définir cette connexion comme la connexion par défaut
+        Container::addConnection($connection);
 
 
         $user = User::where('uid', '=', $uid)->first();
-        
+
 
         if ($user) {
            return dd([
