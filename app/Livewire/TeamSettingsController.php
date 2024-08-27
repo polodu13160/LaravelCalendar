7cce3b9245579be0a619c937446370f6ea723a0d
Clement Couturier <ccouturier@b2pweb.com>
2024-08-27 15:21:50 +0200

    [+] Refactorisation du code depuis AbstractComponent

--- app/Livewire/TeamSettingsController.php
+++ app/Livewire/TeamSettingsController.php
@@ -2,28 +2,14 @@
 
 namespace App\Livewire;
 
-use Livewire\Component;
-
-class TeamSettingsController extends Component
+class TeamSettingsController extends AbstractComponent
 {
-    public $isAdmin;
-
-    public $isModerator;
-
-    public $team;
-
     public function render()
     {
-        $this->team = auth()->user()->currentTeam;
-
-        $this->isAdmin = auth()->user()->isAdmin();
-        // @phpstan-ignore-next-line
-        $this->isModerator = auth()->user()->canDoAction('Moderator', $this->team->id);
-
         return view('livewire.team-settings-controller')->with([
             'team' => $this->team,
-            'isAdmin' => $this->isAdmin,
-            'isModerator' => $this->isModerator,
+            'isAdmin' => $this->isLoggedUserAdmin(),
+            'isModerator' => $this->isLoggedUserModerator(),
         ]);
     }
 }
